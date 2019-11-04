66c2f30fc97e0ea1bf1f7de1240a0d673c3fdf14
Alexander <Terentjev.selogaz@yandex.ru>
2019-11-04 23:12:24 +0700

    Конец 10 урока

--- Lesson 3/js3.js
+++ Lesson 3/js3.js
@@ -8,9 +8,31 @@ function showFirstMessage(text) {
 showFirstMessage("Hello World!");
 console.log(num);*/
 
-function calc(a,b) {
+
+/*function calc(a,b) {
     return (a + b);
-}
+}*/
+
+let calc = (a,b) => a+b
 
 console.log(calc(3,4));
 console.log(calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
