1830a8a00d920b94f2b4c999c011853773de82bd
Alexander <Terentjev.selogaz@yandex.ru>
2019-11-05 14:26:12 +0700

    ООП

--- Massives/mas.js
+++ Massives/mas.js
@@ -40,3 +40,16 @@ function compareNum(a,b) {
 }
 console.log(arr);
 
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let John = {
+    health: 100
+};
+
+John.__proto__ = soldier;
+
+console.log(John);
+console.log(John.armor);
